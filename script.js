342dd57d75856a61220c34468a5d181af2c82340
muchovelli <adamdanda@protonmail.com>
2022-10-26 17:16:45 +0200

    Created header and modified css and navbar js

---
+++ script.js
@@ -0,0 +1,12 @@
+const hamburgerBtn = document.querySelector(".hamburger");
+const navigation = document.querySelector(".navigation");
+const shadow = document.querySelector(".shadow");
+
+let isNavigationActive = false;
+
+hamburgerBtn.addEventListener("click", () => {
+    isNavigationActive = !isNavigationActive;
+    navigation.classList.toggle("active");
+    shadow.classList.toggle("active");
+    document.body.style.overflow = isNavigationActive ? "hidden" : "auto";
+});
