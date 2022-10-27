fbc2df84965fc8ab4269fd646e65573f5ac45c7d
muchovelli <adamdanda@protonmail.com>
2022-10-27 16:16:53 +0200

    Added sidemenu and footer

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ const shadow = document.querySelector(".shadow");
 let isNavigationActive = false;
 
 hamburgerBtn.addEventListener("click", () => {
-    isNavigationActive = !isNavigationActive;
-    navigation.classList.toggle("active");
-    shadow.classList.toggle("active");
-    document.body.style.overflow = isNavigationActive ? "hidden" : "auto";
+  isNavigationActive = !isNavigationActive;
+  hamburgerBtn.classList.toggle("active");
+  navigation.classList.toggle("active");
+  shadow.classList.toggle("active");
+  document.body.style.overflow = isNavigationActive ? "hidden" : "auto";
 });
