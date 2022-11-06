8068024b6c537e8b4c5738eb9e30ea71ddb090ae
muchovelli <adamdanda@protonmail.com>
2022-11-06 16:39:53 +0100

    index.html done

--- js/script.js
+++ js/script.js
@@ -8,6 +8,5 @@ hamburgerBtn.addEventListener("click", () => {
   isNavigationActive = !isNavigationActive;
   hamburgerBtn.classList.toggle("active");
   navigation.classList.toggle("active");
-  shadow.classList.toggle("active");
   document.body.style.overflow = isNavigationActive ? "hidden" : "auto";
 });
