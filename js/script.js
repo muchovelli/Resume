c9e803c4422e15e4ec8eba5c53aa27a8d577f346
muchovelli <adamdanda@protonmail.com>
2022-11-08 18:50:46 +0100

    Done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 const hamburgerBtn = document.querySelector(".hamburger");
 const navigation = document.querySelector(".navigation");
-const shadow = document.querySelector(".shadow");
+const navigationLinks = document.querySelectorAll('.navigation-link');
 
 let isNavigationActive = false;
 
 hamburgerBtn.addEventListener("click", () => {
+  if (isNavigationActive) {
+    hamburgerBtn.classList.remove("active");
+    navigation.classList.remove("active");
+  } else {
+    hamburgerBtn.classList.add("active");
+    navigation.classList.add("active");
+  }
+
   isNavigationActive = !isNavigationActive;
-  hamburgerBtn.classList.toggle("active");
-  navigation.classList.toggle("active");
-  document.body.style.overflow = isNavigationActive ? "hidden" : "auto";
 });
+
+navigationLinks.forEach(el => {
+  el.addEventListener("click", () => {
+    hamburgerBtn.click();
+  })
+})
